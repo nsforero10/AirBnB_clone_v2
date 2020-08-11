029501ed77b8ef069e29564bd1aa3743c663fed1
ffelipegupe <1560@holbertonschool.com>
2020-08-11 03:16:50 +0000

    Update: updating sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,1 +1,7 @@
--- setup_mysql_dev file
+-- Script that prepares a MySQL server for the project
+-- Database, new user, password, privileges.
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* to 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
