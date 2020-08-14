b4cc9a2875596f1f34ef822bb1608ba89f1eb9e1
Nicolas Forero <ns.forero@gmail.com>
2020-08-13 23:22:29 -0500

    updating amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
+from os import getenv
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Implementation for the Amenities"""
+    __tablename__ = 'amenities'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship(
+            "Place", secondary=place_amenity, back_populates="amenities")
+
+    else:
+        name = ""
