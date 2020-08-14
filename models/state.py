ff80d8736e710d1a2b855e447eb460e2156529fd
Nicolas Forero <ns.forero@gmail.com>
2020-08-13 23:31:38 -0500

    Update: updating state to git in db storage

--- models/state.py
+++ models/state.py
@@ -1,10 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """ State class """
+
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+
+    if getenv('HBTN_TYPE_STORAGE') == 'db':
+        cities = relationship(
+            'City', cascade='all, delete-orphan', backref='state')
+
+    else:
+        @property
+        def cities(self):
+            '''Returns list of citiy where dstate_id is equal to State.id'''
+            instance_list = []
+            for _, val in models.storage.all().items():
+                if val.__class__.__name__ == 'City'\
+                        and val.state._id == self.id:
+                    instance_list.append(val)
+            return instance_list
