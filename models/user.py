6fa3dbb99baf45918071c9b9e95a4013ebb60f23
Nicolas Forero <ns.forero@gmail.com>
2020-08-13 23:32:11 -0500

    Update: updating user to fit in new storage DB

--- models/user.py
+++ models/user.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
+import sqlalchemy
 from sqlalchemy import Column, String
-from sqlalchemy import relationship
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", cascade="delete", backref="user")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship(
+            "Place", cascade='all, delete, delete-orphan', backref="user")
+        reviews = relationship(
+            "Review", cascade="all, delete, delete-orphan", backref="user")
+
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
