7761f621f45533ffcc3091d05a8fd1ca5e293e8d
Nicolas Forero <ns.forero10@gmail.com>
2020-08-17 22:57:49 -0500

    [Feature] adding 0 task 0x03

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,6 +6,6 @@ sudo mkdir /data/web_static/shared/
 sudo mkdir /data/web_static/released/test/
 echo "Holberton School" > /data/web_static/release/text/index/html
 sudo ln -s /data/web_static/release/test/ /data/web_static/current
-sudo chmod -R ubuntu:ubuntu /data
+sudo chown -R ubuntu:ubuntu /data
 sudo sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
 sudo service ngingx restart
