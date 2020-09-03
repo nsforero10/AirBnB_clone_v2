5180e7ccdfeb58d6084a81cf63e97b3e4f6e91b7
Nicolas Forero <ns.forero10@gmail.com>
2020-09-02 21:00:18 -0500

    Feature: adding task 3 flask

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+''' script that starts a Flask web application '''
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    ''' Hello holbie '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    ''' holbie '''
+    return 'HBNB'
+
+
+@app.route('/c/<txt>', strict_slashes=False)
+def c_route(txt):
+    ''' C + <txt>'''
+    return 'C {}'.format(txt.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<txt>', strict_slashes=False)
+def python_route(txt='is cool'):
+    ''' Python/(<txt>)'''
+    return 'Python {}'.format(txt.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
