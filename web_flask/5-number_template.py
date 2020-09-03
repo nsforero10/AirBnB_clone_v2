8aef2791c2c9d5e95883db2fecccf7f419daf6fc
Nicolas Forero <ns.forero10@gmail.com>
2020-09-02 22:00:05 -0500

    Feature: adding task 5 flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,48 +1,47 @@
 #!/usr/bin/python3
-''' script that starts a Flask web application '''
+""" Script that starts a Flask web application """
 from flask import Flask, render_template
-
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    ''' Hello holbie '''
-    return 'Hello HBNB!'
+def default_route():
+    """ default_route """
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    ''' holbie '''
-    return 'HBNB'
+def hbnb_route():
+    """hbnb_route """
+    return "HBNB"
 
 
-@app.route('/c/<txt>', strict_slashes=False)
-def c_route(txt):
-    ''' C + <txt>'''
-    return 'C {}'.format(txt.replace('_', ' '))
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """ c_route """
+    return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<txt>', strict_slashes=False)
-def python_route(txt='is cool'):
-    ''' Python/(<txt>)'''
-    return 'Python {}'.format(txt.replace('_', ' '))
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text="is cool"):
+    """ python_route """
+    return "Python {}".format(text.replace("_", " "))
 
 
-@app.route('/number/<int:num>', strict_slashes=False)
-def is_num(num):
-    ''' Is num? '''
-    return '{:d} is a number'.format(num)
-
-
-@app.route('/number_template/<int:num>', strict_slashes=False)
-def number_template(num):
-    ''' Renders 5-number.html '''
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    """ number_route """
     if isinstance(n, int):
-        return render_template('5-number.html', n=n)
+        return "{} is a number".format(n)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template_route(n=None):
+    """ number_template_route """
+    if isinstance(n, int):
+        return render_template("5-number.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run()
